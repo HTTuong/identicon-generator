dabbbcd495722bb377561e518095d7825def5481
Tuong Hoang The <tuong.hoang@tuni.fi>
2024-07-15 11:04:24 +0300

    Add: grid properties for Image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,21 +8,42 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
-  def build_grid(%Identicon.Image{hex: hex_list} = _image) do
-    hex_list
+  def mirror_row(row) do
+      [first, second | _tail] = row
+      row ++ [second, first]
+  end
+
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid = hex_list
     |> Enum.chunk_every(3)
+    |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({value, _index}) ->
+      rem(value, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
       |> :binary.bin_to_list()
+    hex = List.delete_at(hex, length(hex) - 1) #delete the last element ~c"A" in list
 
     %Identicon.Image{hex: hex}
   end
