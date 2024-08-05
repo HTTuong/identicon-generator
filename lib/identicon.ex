033c89de96c92d8344029e1eea5ba1b922525ea5
Tuong Hoang The <tuong.hoang@tuni.fi>
2024-08-05 15:54:18 +0300

    Update: draw identicon image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,6 +10,12 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -17,37 +23,50 @@ defmodule Identicon do
   end
 
   def mirror_row(row) do
-      [first, second | _tail] = row
-      row ++ [second, first]
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
-
   def build_grid(%Identicon.Image{hex: hex_list} = image) do
-    grid = hex_list
-    |> Enum.chunk_every(3)
-    |> Enum.map(&mirror_row/1)
-    |> List.flatten
-    |> Enum.with_index
+    grid =
+      hex_list
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
 
     %Identicon.Image{image | grid: grid}
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    pixel_map = Enum.map grid, fn({_code, index }) ->
-      horizontal = rem(index, 5) * 50
-      vertical = div(index, 5) * 50
-      top_left = {horizontal, vertical }
-      bottom_right = {horizontal + 50, vertical + 50}
-      {top_left, bottom_right}
-    end
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+        {top_left, bottom_right}
+      end)
 
     %Identicon.Image{image | pixel_map: pixel_map}
   end
 
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    grid = Enum.filter grid, fn({value, _index}) ->
-      rem(value, 2) == 0
-    end
+    grid =
+      Enum.filter(grid, fn {value, _index} ->
+        rem(value, 2) == 0
+      end)
 
     %Identicon.Image{image | grid: grid}
   end
@@ -56,7 +75,9 @@ defmodule Identicon do
     hex =
       :crypto.hash(:md5, input)
       |> :binary.bin_to_list()
-    hex = List.delete_at(hex, length(hex) - 1) #delete the last element ~c"A" in list
+
+    # delete the last element ~c"A" in list
+    hex = List.delete_at(hex, length(hex) - 1)
 
     %Identicon.Image{hex: hex}
   end
